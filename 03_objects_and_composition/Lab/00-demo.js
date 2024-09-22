3c0a3101b2695a0200ecd98c206e8bb53fa80e2a
todorovboris <btodorovtodorov@gmail.com>
2024-09-22 08:51:58 +0300

    demo file created

--- 03_objects_and_composition/Lab/00-demo.js
+++ 03_objects_and_composition/Lab/00-demo.js
@@ -5,4 +5,47 @@
 
 //-------------------------------------------------------------------------------------------------
 
+// Методи(функции) и контекст
+
+// 1) замяна и повече ползван похват вместо Swtich:
+function switchh(command) {
+    let count1 = 5;
+    switch (command) {
+        case 'increment':
+            count1++;
+            break;
+        case 'decrement':
+            count1--;
+            break;
+        case 'reset':
+            count1 = 0;
+            break;
+    }
+
+    console.log(count1);
+}
+
+function methods(command) {
+    let count2 = 5;
+
+    const check = {
+        increment() {
+            count2++;
+        },
+        decrement() {
+            count2--;
+        },
+        reset() {
+            count2 = 0;
+        },
+    };
+    check[command]();
+    console.log(count2);
+}
+
+switchh('increment'); // => връща 6
+methods('decrement'); // => връща 4;
+
+//-------------------------------------------------------------------------------------------------
+
 // Композиция
