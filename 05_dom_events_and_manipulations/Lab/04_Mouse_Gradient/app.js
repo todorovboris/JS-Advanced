ef6035cc26d0f29b5aa1529c9bb9d78d995b385b
Boris Todorov <btodorovtodorov@gmail.com>
2024-10-02 12:59:23 +0300

    tasks 3-7 added

--- 05_dom_events_and_manipulations/Lab/04_Mouse_Gradient/app.js
+++ 05_dom_events_and_manipulations/Lab/04_Mouse_Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientBox = document.getElementById('gradient');
+    const result = document.getElementById('result');
+
+    gradientBox.addEventListener('mousemove', (e) => {
+        const mousePosition = e.offsetX;
+        const boxWidth = gradientBox.clientWidth;
+
+        const percent = Math.floor((mousePosition / boxWidth) * 100);
+
+        result.textContent = `${percent}%`;
+    });
 }
