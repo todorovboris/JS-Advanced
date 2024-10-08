a3cd7dcb4ec94db6055501f66d7b596d025e04fd
Boris Todorov <btodorovtodorov@gmail.com>
2024-10-08 20:12:36 +0300

    task 6 added

--- 05_dom_events_and_manipulations/Exe/06. Furniture/solution.js
+++ 05_dom_events_and_manipulations/Exe/06. Furniture/solution.js
@@ -1,8 +1,12 @@
 function solve() {
-    const buttonGenerate = document.querySelector('button');
+    const buttons = document.querySelectorAll('button');
+    const generateBtn = buttons[0];
+    const buyBtn = buttons[1];
 
-    buttonGenerate.addEventListener('click', (e) => {
-        const inputElm = document.querySelector('textarea');
+    const inputElm = document.querySelectorAll('textarea')[0];
+    const outputElm = document.querySelectorAll('textarea')[1];
+
+    generateBtn.addEventListener('click', (e) => {
         const data = JSON.parse(inputElm.value);
 
         const productListElm = document.querySelector('table.table tbody');
@@ -33,5 +37,36 @@ function solve() {
 
             productListElm.appendChild(product);
         });
+    });
+
+    buyBtn.addEventListener('click', (e) => {
+        const data = [];
+
+        const dataArr = document.querySelectorAll('.table tbody tr').forEach((item) => {
+            if (item.querySelector('input:checked')) {
+                const cells = item.querySelectorAll('td');
+                data.push({
+                    name: cells[1]?.textContent,
+                    price: cells[2]?.textContent,
+                    decFactor: cells[3]?.textContent,
+                });
+            }
+        });
+
+        const totalPrice = data.reduce((sum, item) => {
+            return sum + Number(item.price);
+        }, 0);
+
+        const totalDecFactor = data.reduce((sum, item) => {
+            return sum + Number(item.decFactor);
+        }, 0);
+
+        const avgDecFactor = totalDecFactor / data.length;
+
+        let output = `Bought furniture: ${data.map((item) => item.name).join(', ')}\n`;
+        output += `Total price: ${totalPrice.toFixed(2)}\n`;
+        output += `Average decoration factor: ${avgDecFactor}`;
+
+        outputElm.value = output;
     });
 }
