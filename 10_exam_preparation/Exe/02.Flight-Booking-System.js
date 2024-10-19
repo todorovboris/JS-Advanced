d7a45708c844b1fb68e7619b950008ca923d445e
Boris Todorov <btodorovtodorov@gmail.com>
2024-10-19 22:18:49 +0300

    task 1 added

---
+++ 10_exam_preparation/Exe/02.Flight-Booking-System.js
@@ -0,0 +1,24 @@
+class FlightBookingSystem {
+    constructor(agencyName) {
+        this.agencyName = agencyName;
+        this.flights = [];
+        this.bookings = [];
+        this.bookingsCount = 0;
+    }
+
+    addFlight(flightNumber, destination, departureTime, price) {
+        //
+    }
+
+    bookFlight(passengerName, flightNumber) {
+        //
+    }
+
+    cancelBooking(passengerName, flightNumber) {
+        //;
+    }
+
+    showBookings(criteria) {
+        //
+    }
+}
