64993c0bacbb9c4279acaa418846a2e42aa39f19
Boris Todorov <btodorovtodorov@gmail.com>
2024-10-20 11:15:16 +0300

    tasks 2 and 3 added

--- 10_exam_preparation/Exe/03.Online-Store_Resources/onlineStore.js
+++ 10_exam_preparation/Exe/03.Online-Store_Resources/onlineStore.js
@@ -1,47 +1,47 @@
-const onlineStore = {
+export const onlineStore = {
     isProductAvailable(product, stockQuantity) {
-    if (typeof product !== "string" || typeof stockQuantity !== "number") {
-            throw new Error("Invalid input.");
+        if (typeof product !== 'string' || typeof stockQuantity !== 'number') {
+            throw new Error('Invalid input.');
         }
-        
-      if (stockQuantity <= 0) {
-        return `Sorry, ${product} is currently out of stock.`;
-      } else {
-        return `Great! ${product} is available for purchase.`;
-      }
+
+        if (stockQuantity <= 0) {
+            return `Sorry, ${product} is currently out of stock.`;
+        } else {
+            return `Great! ${product} is available for purchase.`;
+        }
     },
     canAffordProduct(productPrice, accountBalance) {
-      if (typeof productPrice !== "number" || typeof accountBalance !== "number") {
-        throw new Error("Invalid input.");
-      }
-  
-      let remainingBalance = accountBalance - productPrice;
-  
-      if (remainingBalance < 0) {
-        return "You don't have sufficient funds to buy this product.";
-      } else {
-        return `Product purchased. Your remaining balance is $${remainingBalance}.`;
-      }
+        if (typeof productPrice !== 'number' || typeof accountBalance !== 'number') {
+            throw new Error('Invalid input.');
+        }
+
+        let remainingBalance = accountBalance - productPrice;
+
+        if (remainingBalance < 0) {
+            return "You don't have sufficient funds to buy this product.";
+        } else {
+            return `Product purchased. Your remaining balance is $${remainingBalance}.`;
+        }
     },
     getRecommendedProducts(productList, category) {
-      let recommendedProducts = [];
-  
-      if (!Array.isArray(productList) || typeof category !== "string") {
-        throw new Error("Invalid input.");
-      }
-  
-      productList.forEach((product) => {
-        if (product.category === category) {
-          recommendedProducts.push(product.name);
+        let recommendedProducts = [];
+
+        if (!Array.isArray(productList) || typeof category !== 'string') {
+            throw new Error('Invalid input.');
         }
-      });
-  
-      if (recommendedProducts.length === 0) {
-        return `Sorry, we currently have no recommended products in the ${category} category.`;
-      } else {
-        return `Recommended products in the ${category} category: ${recommendedProducts.join(", ")}`;
-      }
+
+        productList.forEach((product) => {
+            if (product.category === category) {
+                recommendedProducts.push(product.name);
+            }
+        });
+
+        if (recommendedProducts.length === 0) {
+            return `Sorry, we currently have no recommended products in the ${category} category.`;
+        } else {
+            return `Recommended products in the ${category} category: ${recommendedProducts.join(', ')}`;
+        }
     },
-  };
-  
-  module.exports = onlineStore;
+};
+
+// module.exports = onlineStore;
